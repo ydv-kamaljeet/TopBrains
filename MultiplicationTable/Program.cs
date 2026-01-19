fb4e0f2468eb4fdded93d98c3b719f14498b71ea
kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2026-01-19 10:35:07 +0530

    MultiplicationTable Task Completed.

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Question2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Enter Number N : ");
+                if (!int.TryParse(Console.ReadLine(), out int n))
+                    throw new Exception();
+
+                Console.Write("Enter Number Upto : ");
+                if (!int.TryParse(Console.ReadLine(), out int upto))
+                    throw new Exception();
+
+                int[] multiplicationRow = Multiplication(n, upto);
+
+                foreach (int item in multiplicationRow)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            catch (Exception ecp)
+            {
+                Console.WriteLine($"Enter Valid Input.\nError Message : {ecp.Message}");
+            }
+
+        }
+
+
+        public static int[] Multiplication(int n, int upto)
+        {
+            int[] multiplicationRow = new int[upto];
+            for (int i = 1; i <= upto; i++)
+            {
+                multiplicationRow[i - 1] = n * i;
+            }
+
+            return multiplicationRow;
+        }
+    }
+}
