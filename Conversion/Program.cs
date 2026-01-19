fcfd2b1a1a546845c56fd9c6d7d6e825eac17d12
kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2026-01-19 12:20:36 +0530

    Question 18 : Conversion Task Completed

---
+++ Conversion/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Question18
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(!Decimal.TryParse(Console.ReadLine(),out decimal foot))  //taking valid input of decimal
+                Console.WriteLine("Enter valid Input for Foot.");
+            
+            decimal cm = foot * 30.48m; //foot to cm conversion
+            decimal roundOffCm =  Math.Round(cm, 2, MidpointRounding.AwayFromZero); //rounding off to 2 decimal place
+            Console.WriteLine($"{foot} Foot = {roundOffCm} cm");
+        }
+    }
+}
