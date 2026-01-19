428c6db3b418b83fe38013c9884a6f4e7654c123
kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2026-01-19 12:31:47 +0530

    Question 7 : Display Height Task completed

---
+++ DisplayHeight/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Question7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(!int.TryParse(Console.ReadLine(),out int HeightCm))  //Taking input
+                Console.WriteLine("enter Valid Height");
+            
+            //Checking Height Category
+            if(HeightCm<150)
+                Console.WriteLine("Short");
+            else if(HeightCm>=150 && HeightCm<180)
+                Console.WriteLine("Average");
+            else if(HeightCm>=180)
+                Console.WriteLine("tall");
+        }
+    }
+}
