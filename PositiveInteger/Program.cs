d3c29250699ceb1a0e7c39e36e0b849ca4f96c8a
kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2026-01-21 10:48:05 +0530

    Question 12 : Sum of Positive Integer in Array Task Completed

---
+++ PositiveInteger/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Question12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            int[] arr = {1,-2,5,9,-34,0,33,-12};
+            Console.WriteLine($"Sum : {SumOfPositiveInteger(arr)}");
+            
+        }
+
+        public static int SumOfPositiveInteger(int[] arr)
+        {
+            int sum=0;
+            foreach(int it in arr)
+            {
+                if (it > 0)
+                {
+                    sum+=it;
+                }else if(it == 0)
+                {
+                    break;
+                }
+            }
+            return sum;
+        }
+
+    }
+}
