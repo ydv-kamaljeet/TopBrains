128e3c980a697e1cce04deef5f12225caa13a52d
kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2026-01-21 10:38:16 +0530

    Question 8 : Bank Transaction Task Completed

---
+++ BankTransaction/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Question8;
+public class Program
+{
+    public static int FinalBalance(int initialBalance, int[] transactions)
+    {
+        int balance = initialBalance;
+
+        foreach (int t in transactions)
+        {
+            if (t >= 0)
+            {
+                balance += t; 
+            }
+            else if (balance + t >= 0)
+            {
+                balance += t;
+            }
+        }
+
+        return balance;
+    }
+    public static void Main()
+    {
+        int initialBalance = 1000;
+        int[] transactions = { 200, -500, -800, 300 };
+
+        int result = FinalBalance(initialBalance, transactions);
+        Console.WriteLine(result);
+    }
+}
