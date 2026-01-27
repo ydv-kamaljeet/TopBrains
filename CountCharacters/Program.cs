0a76410a9cb865cda2f3476e49458f4939d88093
kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2026-01-27 14:34:27 +0530

    Question 19 : Count characters of string task completed

---
+++ CountCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Question19
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int Len=0;
+            string input = Console.ReadLine() ?? string.Empty;
+            foreach(char character in input)
+            {
+                if(character != ' ')    
+                    Len++;
+            }
+            Console.WriteLine($"Total Characters in this string is : {Len} (Exculding white spaces)");
+        }
+    }
+}
+/* 
+2nd Solution:
+
+instead of checking white space everytime , we remove the whitespace and then count the characters.
+>> string withoutSpaces = input.Replace(" ", "");
+>> Now we can count the characters in withoutSpaces string and return it.
+
+*/
